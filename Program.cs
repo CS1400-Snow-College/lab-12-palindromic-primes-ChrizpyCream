6180ed9ba473368d34f8ffd18e8eba7e58bbae71
Christopher Hercules <chriherc215@gmail.com>
2024-12-04 15:17:01 -0700

    nth

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics;
+Console.Clear();
+Console.WriteLine("This will help you find the Nth Palindromic Prime.");
+Console.WriteLine("To start, enter any number for the nth prime you would like to see.");
+Console.WriteLine("What Nth Palindromic Prime would you like to see\n");
+Console.WriteLine(NthPalindromicPrime(ReadNumber()));
+
+
+long NthPalindromicPrime(long n)
+{
+ long Nth = 0;
+ int i = 1;
+ int x = 0;
+   do
+    {
+       i++;
+       if(isPrime(i) == true && isPalindromic(i) == true)
+       {
+           x++;
+           if(x > n)
+           {
+             break;
+           }
+            Nth = i;
+        }
+    }while(true);
+    return Nth;
+}
+
+
+// checks if the number is prime
+bool isPrime (long number)
+{
+    double square = Math.Sqrt(number);
+    for(int i = 2; i <= square; i++)
+    {
+        if(number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+
+// checks if the number is palindromic
+bool isPalindromic(long number)
+{
+    string NumString = number.ToString();
+    if(writeBackwards(NumString) == NumString)
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+    
+}
+
+
+long ReadNumber()
+{
+    long number = -1;
+    do
+    {
+        try
+        {
+            Console.Write("Please enter a number ");
+            string num = Console.ReadLine();
+            number = Convert.ToInt64(num);
+            return number;
+        }catch(FormatException)
+        {
+            number = -1;
+            
+        }
+    }while(number == -1);
+    return number;
+}
+
+
+string writeBackwards(string Forward)
+{
+    List<char> Char_acters = new List<char>();
+    string newString = "";
+    foreach(char a in Forward)
+    {
+        Char_acters.Add(a);
+    }
+    Char_acters.Reverse();
+    for(int i = 0; i<Char_acters.Count; i++)
+    {
+        
+        newString = newString + Char_acters[i];
+    }
+    return newString;
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Lab-10-Pallindromic-Primes
 
 Please read the write up available in canvas
+
+Hints
+Recall that we can use the modulus operator to see if a number is evenly divisible by another number. E.g. 104 % 7 != 0 so that means that 104 is not divisible by 7, but 104 % 4 == 0 so that means that 104 is divisible by 4.
+Remember that the ToString() method will create a string that looks like a particular integer. In other words: 34.ToString() == "34".
+Recognize that you don't necessarily need to store the palindromic primes in order to find the nth one. Instead, you can just keep track of how many you have found (e.g. with an integer counter). There are other things you can do to speed things up (or to practice), but those are optional (see below only if you are interested).
